2f38d80c254f356c57c534cbb95ac4562b2a125e
Damrod14 <erickrodriguez932@gmail.com>
2023-03-28 00:31:51 -0600

    hacemos uso de las clases creadas en el "main"

--- main.java
+++ main.java
@@ -10,13 +10,16 @@ public class main{
         System.out.print("Ingrese la cadena a analizar: ");
         String cadena = lector.nextLine();//guardamos la informacion en "cadena"
 
-        System.out.println(cadena);
-        
-        
+        A_lex analizador = new A_lex(cadena);
+        List<Token> tokens = analizador.listaToken();
+        for(Token token : tokens) {
+
+            System.out.println(token.getEtiqueta() +":"+ token.getValor());
+        }
         
         lector.close();  
     }
-    
+
 }
 
 class Token{ //creamos clase Token con dos variables privadas, etiqueta y valor
@@ -43,21 +46,21 @@ class A_lex{
         this.entrada = entrada; //creamos la propiedad y asignamos el valor entrada
     }
 
-    public List<Token> ListaToken(){
+    public List<Token> listaToken(){//creamos una lista con los tokens 
         List<Token> tokens = new ArrayList<>();
 
         String[] cadena = entrada.split(" "); //dividmos la entrada en palabras y se añaden al arreglo
         for (String palabra : cadena) {//recorremos todos los elementos del arreglo palabra para compararlos y etiquetarlos
             
-            if (palabra.matches("[a-zA-Z_][a-zA-Z0-9_]*")){//definimos una expresión regular la cual indica como puede comenzar la cadena de entrada, "*" indica cero o mas veces
-                tokens.add(new Token("Identificador", palabra)); 
+            if (palabra.matches("[a-zA-Z_(][a-zA-Z0-9_)-]*")){//definimos una expresión regular la cual indica como puede comenzar la cadena de entrada, "*" indica cero o mas veces
+                tokens.add(new Token("String", palabra)); 
             }
 
             else if (palabra.matches("[0-9]+")){//expresion regular para etiquetar los numeros del 0 al 9 el "+" es para indicar "uno o más"
                 tokens.add(new Token("Int", palabra));
             } 
 
-            else if (palabra.matches("[+\\-*/=]")){
+            else if (palabra.matches("[+\\-*/=]")){//expresion regular para etiquetar los simbolos operadores, el "\\ es para indicar que los simbolos siguientes se busquen tal cual y no los interprete como caracter especial"
                 tokens.add(new Token("Operador", palabra));
             }
         }
