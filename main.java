6f93e55b2b04029c28df9f2b63f2c8df7ef470d3
Damrod14 <erickrodriguez932@gmail.com>
2023-03-27 20:51:16 -0600

    solicitamos cadena

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class main{
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hola Mundo"); 
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in); //lector leera la cadena ingresada por el usuario
+        
+        System.out.print("Ingrese la cadena a analizar: ");
+        String cadena = lector.nextLine();
+
+        System.out.println(cadena);
         
     }
 }
