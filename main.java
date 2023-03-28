bfcae4ce284f551e25076c7985b40f12b9a387ba
Damrod14 <erickrodriguez932@gmail.com>
2023-03-27 21:25:54 -0600

    Creación de clase Token

--- main.java
+++ main.java
@@ -6,9 +6,27 @@ public class main{
         Scanner lector = new Scanner(System.in); //lector leera la cadena ingresada por el usuario
         
         System.out.print("Ingrese la cadena a analizar: ");
-        String cadena = lector.nextLine();
+        String cadena = lector.nextLine();//guardamos la informacion en "cadena"
 
         System.out.println(cadena);
         
+    }
+    
+}
+
+class Token{ //creamos clase Token con dos variables privadas, etiqueta y valor
+    private String etiqueta;
+    private String valor;
+    public Token(String etiqueta, String valor){
+        this.etiqueta = etiqueta;//asignamos etiqueta a la propiedad etiqueta
+        this.valor = valor;//asignamos valor a la propiedad valor
+    }
+
+    public String getEtiqueta(){//obtenermos el dato etiqueta
+        return etiqueta;//devolvemos el dato etiqueta
+    }
+
+    public String getValor(){//obtenermos el dato de valor
+        return valor;//devolvemos el dato valor
     }
 }
